280dca508efaeb4807458a02bee4a23b89ba9052
SiD-026 <sidysid26@gmail.com>
2023-04-06 22:49:59 +0530

    package file alttered

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
-        * <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route exact path="/profile" element={<Profile />} />
       </Routes>
     </Router>
